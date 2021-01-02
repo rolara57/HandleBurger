0803ab7a0a6fd875f4b8a8876e4421c4a0d12853
rolara57 <roger@qm46.com>
2021-01-01 21:29:23 -0600

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bacon', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Veggie', true);
